e4ac8d775858fe945e295c504e0133ebce3c42b8
Nico <nico.ritsche@gmail.com>
2013-11-28 15:43:05 +0100

    add class_name parameter

--- lib/nested_form_fields.rb
+++ lib/nested_form_fields.rb
@@ -16,7 +16,6 @@ module ActionView::Helpers
       fields_options[:builder] ||= options[:builder]
       fields_options[:parent_builder] = self
       fields_options[:wrapper_tag] ||= :fieldset
-      fields_options[:legend] ||= nil
       fields_options[:namespace] = fields_options[:parent_builder].options[:namespace]
 
       return fields_for_has_many_association_with_template(record_name, record_object, fields_options, block)
@@ -62,7 +61,7 @@ module ActionView::Helpers
 
     def nested_model_template name, association_name, options, block
       for_template = self.options[:for_template]
-      
+
       # Render the outermost template in a script tag to avoid it from being submited with the form
       # Render all deeper nested templates as hidden divs as nesting script tags messes up the html.
       # When nested fields are added with javascript by using a template that contains nested templates,
@@ -75,8 +74,9 @@ module ActionView::Helpers
                              class: for_template ? 'form_template' : nil,
                              style: for_template ? 'display:none' : nil ) do
         nested_fields_wrapper(association_name, options[:wrapper_tag], options[:legend]) do
+          association_class = (options[:class_name] || association_name).to_s.classify.constantize
           fields_for_nested_model("#{name}[#{index_placeholder(association_name)}]",
-                                   association_name.to_s.classify.constantize.new,
+                                   association_class.new,
                                    options.merge(for_template: true), block)
         end
       end
