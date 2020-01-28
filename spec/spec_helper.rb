b1fe8e831b49c681a0b032f53a772adadbb609ec
Will Leonard <will.leonard89@gmail.com>
2020-01-27 21:50:03 -0500

    Start cleaning up some deprecations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,6 @@ require 'capybara/rails'
 Capybara.default_selector  = :css
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.include AssertDifference
   config.use_transactional_fixtures = true
